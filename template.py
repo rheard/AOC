0e344d22ff61cedb4844f3d38e9ec3622d998824
Ryan Heard <ryanwheard@gmail.com>
2025-12-02 00:42:30 -0500

    Add copy-pastable template that I've been starting with

---
+++ template.py
@@ -0,0 +1,11 @@
+# Step 1: Load data and format into data structure:
+with open("p01.txt") as rb:
+    rb.readlines()
+
+
+# Step 2: Solve the problem
+answer = 0
+
+
+# Step 3: Output answer
+print(answer)
